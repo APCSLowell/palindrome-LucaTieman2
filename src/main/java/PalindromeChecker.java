b38b0ff4ce4cf6a268980b55af2fc65d39c6382b
LucaTieman2 <143014925+LucaTieman2@users.noreply.github.com>
2024-02-07 10:09:33 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -35,22 +35,18 @@ public void tester()
     }
   }
 }
-public boolean palindrome(String word)
-{
-  //your code here
-  String backwards = reverse(word);
-  if (backwards.substring(0).equals(word.substring(0))) {
-    return true;  
+public boolean palindrome(String sWord) {
+  if (onlyLetters(noSpaces(word)).equals(onlyLetters(reverse(noSpaces(word))))) {
+    return true;
   } else {
     return false;
   }
 }
-public String reverse(String str)
-{
-    String sNew = new String();
-    for (int i = str.length(); i > 0; i--) {
-      sNew = sNew + str.substring(i-1, i);
-    }
-    return str;
+public String reverse(String str) {
+  String sNew = new String();
+  for (int i = str.length()-1; i > 0; i--) {
+    sNew = sNew + str.charAt(i);
+  }
 }
+
 }
