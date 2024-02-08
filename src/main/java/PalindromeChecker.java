09705b335d78131b765203804aa76e5f1d2afd1c
LucaTieman2 <143014925+LucaTieman2@users.noreply.github.com>
2024-02-08 11:11:24 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -36,17 +36,17 @@ public void tester()
   }
 }
 public boolean palindrome(String sWord) {
-  if (onlyLetters(noSpaces(word)).equals(onlyLetters(reverse(noSpaces(word))))) {
-    return true;
-  } else {
-    return false;
-  }
+  if (onlyLetters(noSpaces(word)).equals(onlyLetters(reverse(noSpaces(word))))) 
+  return true;
+  else 
+  return false;
 }
 public String reverse(String str) {
   String sNew = new String();
-  for (int i = str.length()-1; i > 0; i--) {
+  for (int i = str.length()-1; i >= 0; i--) {
     sNew = sNew + str.charAt(i);
   }
+  return sNew;
 }
 public String noSpaces(String sWord) {
   String answer = "";
